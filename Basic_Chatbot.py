c077bf4691c7079e60cb763170b9d33daf077eb2
Pranav-Surwase <surwasep05@gmail.com>
2026-01-06 22:25:02 +0530

    Add predefined chatbot responses using if-elif conditions.

--- Basic_Chatbot.py
+++ Basic_Chatbot.py
@@ -1,3 +1,11 @@
 # Basic Rule-Based Chatbot
 def chatbot_response(user_input):
 user_input = user_input.lower()
+if user_input == "hello":  
+    return "Hi! 👋"  
+elif user_input == "how are you":  
+    return "I'm fine, thanks! 😊"  
+elif user_input == "bye":  
+    return "Goodbye! 👋 Have a nice day!"  
+else:  
+    return "Sorry, I don't understand that"
